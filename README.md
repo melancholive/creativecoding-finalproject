676cd3ce000bfa7d9638232945b3294e04d90028
Si Yue Jiang <sj3834@nyu.edu>
2022-12-05 14:30:26 -0500

    1st Push
    Setting up some classes, collecting assets, etc

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # creativecoding_final_siyue
 this is my creative coding final project :D
+
+roundtable discussion presentation link:
+https://docs.google.com/presentation/d/1IG3cGuMi-Nf5PJ8hsVyR4BJOI9pOCEFFtJ89YYlNJz8/edit?usp=sharing
+
+sound assets used:
+forest ambience: https://www.youtube.com/watch?v=EUer-Tto1ZA&t=1875s
+grass sound effect: https://youtu.be/4eVmKIfZFpY
--- p5_starterCode/sketch.js
+++ p5_starterCode/sketch.js
@@ -1,14 +1,93 @@
+// Si Yue Jiang -- Creative Coding F22 Final Project
+
+let p; // player character
+let butterflies = [];
+
+let counter = 0;
+
+let forestAmbience;
+let grassSoundEffect;
+
+let bg;
+let grass;
+let bf; // butterfly
+
+function preload(){
+  // sound files
+  forestAmbience = loadSound('forest-ambience.mp3');
+  grassSoundEffect = loadSound('grass-sound-effect.mp3');
+
+  // image files
+  bg = loadImage('dark-forest.jpg');
+  grass = loadImage('grass.png')
+  bf = loadImage('butterfly.png');
+}
+
 function setup() {
-  // put setup code here
-  createCanvas(800,800);
-  background(0);
-  
+  createCanvas(1800,900);
+  forestAmbience.play();
+
+  p = new Player();
 }
 
 function draw() {
-  // put drawing code here
-  fill(117);
-  stroke(200,127,5);
-  strokeWeight(1);
-  rect(width/2 - 100, height/2 - 100, 200,200)
+  counter++;
+
+  background(bg);
+
+  // ground placeholder
+  fill(0);
+  stroke(255);
+  strokeWeight(3);
+  rect(-10, height-200, width+10,200)
+
+  image(grass, 0, 400); // https://p5js.org/examples/image-load-and-display-image.html
+
+  // automated butterfly spawns
+  if ( counter % 100 == 0 ){
+    butterflies.push(new Butterfly());
+  }
+
+  for ( let i = butterflies.length - 1; i >= 0; i-- ){
+    let b = butterflies[i];
+    b.drawButterfly();
+    b.updateButterfly();
+  }
+
+  p.drawPlayer();
+  p.updatePlayer();
+}
+
+class Player{
+  constructor(){
+    this.position = createVector(width/2,height-400)
+  }
+
+  drawPlayer(){
+    rect(this.position.x, this.position.y-50);
+  }
+
+  updatePlayer(){
+
+  }
+}
+
+class Butterfly{
+  constructor(){
+    this.position = createVector(random(0,width),random(0,height));
+    this.timer = random(-20,20);
+  }
+
+  drawButterfly(){
+    if ( this.timer < 100 ){
+      tint(255,255-this.timer*2); // https://p5js.org/examples/image-transparency.html
+      image(bf, this.position.x, this.position.y, bf.width/2, bf.height/2);
+    }
+  }
+
+  updateButterfly(){
+    this.timer++;
+    this.position.x = this.position.x + random(-5,5);
+    this.position.y = this.position.y + random(-5,5);
+  }
 }
